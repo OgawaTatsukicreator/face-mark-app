3ebe08b231ee2294f44c26237e44328ccc7bcd7c
OgawaTatsukicreator <ogawatatuki291@gmail.com>
2026-09-04 21:39:02 +0900

    環境変数の適応をしてAPIキーの漏洩対策をしました

--- test-detect.mjs
+++ test-detect.mjs
@@ -1,9 +1,14 @@
 import { readFileSync } from "node:fs";
+import "dotenv/config";
 
-const API_URL = "FACE_API_URL"
-const API_KEY = "FACE_API_KEY"; // 後で環境変数化する
+const API_URL = process.env.FACE_API_URL;
+const API_KEY = process.env.FACE_API_KEY;
 
 async function main() {
+    if (!API_URL || !API_KEY) {
+        throw new Error("環境変数 FACE_API_URL / FACE_API_KEY が設定されていません");
+    }
+
     const fileBuffer = readFileSync("./face.jpg");
     const blob = new Blob([fileBuffer])
 
@@ -12,9 +17,7 @@ async function main() {
 
     const response = await fetch(API_URL, {
         method: "POST",
-        headers: {
-            "x-api-key": API_KEY,
-        },
+        headers: { "x-api-key": API_KEY },
         body: formData,
     });
 
